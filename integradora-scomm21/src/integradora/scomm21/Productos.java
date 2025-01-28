b4e4a9b327df2fb35f33fca498eda5691bd1486d
Uriel Abraham <abraham2160@hotmail.com>
2025-01-28 13:46:24 -0700

    Creacion del proyecto

---
+++ integradora-scomm21/src/integradora/scomm21/Productos.java
@@ -0,0 +1,15 @@
+package integradora.scomm21;
+public class Productos {
+    public Productos(){}
+    public void agregarProductos(String nombre, String seccion, int unidades,
+            int precio){
+        
+    }
+    public void eliminarProductos(String nombre, String seccion, int cantidad){
+        
+    }
+    public void actualizarInventario(String producto, String seccion, 
+            int cantidad){
+        
+    }
+}
